caab41d9114c0b37f94c58467bfa1ecb36b46034
joosung.park <sdop25@gmail.com>
2020-12-02 05:17:09 +0900

    안드로이드 샘플 앱 작성

--- androidApp/src/main/java/com/sdop/kmmsample/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/sdop/kmmsample/androidApp/MainActivity.kt
@@ -4,13 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.sdop.kmmsample.shared.SpaceXSDK
+import com.sdop.kmmsample.shared.cache.DatabaseDriverFactory
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var launchesRecyclerView: RecyclerView
     private lateinit var progressBarView: FrameLayout
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
+    private val launchesRecyclerViewAdapter = LaunchesRecyclerViewAdapter(listOf())
+
+    private val sdk = SpaceXSDK(DatabaseDriverFactory(this))
+
+    private val mainScope = MainScope()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +35,36 @@ class MainActivity : AppCompatActivity() {
         launchesRecyclerView = findViewById(R.id.launchesListRecyclerView)
         progressBarView = findViewById(R.id.progressBar)
         swipeRefreshLayout = findViewById(R.id.swipeContainer)
+
+        launchesRecyclerView.adapter = launchesRecyclerViewAdapter
+        launchesRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        swipeRefreshLayout.setOnRefreshListener {
+            swipeRefreshLayout.isRefreshing = false
+            displayLaunches(true)
+        }
+
+        displayLaunches(false)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mainScope.cancel()
+    }
+
+    private fun displayLaunches(needReload: Boolean) {
+        progressBarView.isVisible = true
+        mainScope.launch {
+            kotlin.runCatching {
+                sdk.getLaunches(needReload)
+            }.onSuccess {
+                launchesRecyclerViewAdapter.launches = it
+                launchesRecyclerViewAdapter.notifyDataSetChanged()
+            }.onFailure {
+                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+            progressBarView.isVisible = false
+        }
+    }
+
 }
